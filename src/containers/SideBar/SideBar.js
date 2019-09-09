37cf986023589be339ce0eda6285449d2ea09d7f
gionellef <gionellef@gmail.com>
2019-09-09 13:58:05 +0800

    Added SideBarHeader component

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,24 +1,38 @@
 import React from 'react';
 
-import { Menu } from 'semantic-ui-react';
+import { Menu, Divider } from 'semantic-ui-react';
 
 import './SideBar.scss';
 import { SideBarItem } from './SideBarItem/SideBarItem';
+import { SideBarHeader } from './SideBarHeader/SideBarHeader';
 
 export class SideBar extends React.Component {
   render() {
     return (
       <Menu borderless vertical stackable fixed='left' className='side-nav'>
-        <SideBarItem highlight={true} label='Home' icon='home' />
+        <SideBarItem highlight={ true } label='Home' icon='home' />
         <SideBarItem label='Trending' icon='fire' />
         <SideBarItem label='Followers' icon='spy' />
+        
+        <Divider />
+        
+        <SideBarHeader title='Library' />
         <SideBarItem label='History' icon='history' />
         <SideBarItem label='Watch later' icon='clock' />
         <SideBarItem label='Liked videos' icon='thumbs up' />
+
+        <Divider />
+        
+        <SideBarHeader title='More from youtube' />
         <SideBarItem label='Movies and Shows' icon='film' />
+
+        <Divider />
+
         <SideBarItem label='Report history' icon='flag' />
         <SideBarItem label='Help' icon='help circle' />
         <SideBarItem label='Send feedback' icon='comment' />
+
+        <Divider />
       </Menu>
     );
   }
