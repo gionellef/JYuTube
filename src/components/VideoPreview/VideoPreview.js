f2dc70dbc525b666efe5ae47325fdfcd2bc9cae8
gionellef <gionellef@gmail.com>
2019-09-11 14:17:54 +0800

    Complete mockup of VideoPreview component

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -13,6 +13,14 @@ export class VideoPreview extends React.Component {
             <span>5:22</span>
           </div>
         </div>
+        
+        <div className='video-info'>
+          <div className='semi-bold show-max-two-lines'>Video Title</div>
+          <div className='video-preview-metadata-container'>
+            <div className='channel-title'>Channel Title</div>
+            <div><span>2.1M views • 2 days ago</span></div>
+          </div>
+        </div>
       </div>
     );
   }
