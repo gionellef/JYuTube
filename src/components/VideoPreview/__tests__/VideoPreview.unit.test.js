e072435aa33ff3acf3744c27e46c4052313f8e78
gionellef <gionellef@gmail.com>
2019-09-12 11:26:23 +0800

    Enable horizontal videopreview for watch view related videos

--- src/components/VideoPreview/__tests__/VideoPreview.unit.test.js
+++ src/components/VideoPreview/__tests__/VideoPreview.unit.test.js
@@ -5,8 +5,14 @@ import { VideoPreview } from '../VideoPreview';
 
 
 describe('VideoPreview', () => {
-  test('renders VideoPreview', () => {
+  test('renders VideoPreview vertical / default', () => {
     const wrapper=shallow(<VideoPreview />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  test('renders VideoPreview horizontal', () => {
+    const wrapper = shallow(<VideoPreview hoizontal={true}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
 });
--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 import { Video } from '../../components/Video/Video';
+import { VideoPreview } from '../../components/VideoPreview/VideoPreview';
 
 import './Watch.scss';
 
 export class Watch extends React.Component {
   render() {
     return (
-      <div style={{maxWidth: '80%'}}>
-        <Video id='P5ktQh3sWZk' />
+      <div className='watch-component'>
+        <VideoPreview />
+        <VideoPreview horizontal={false} />
+        <VideoPreview horizontal={true} />
+        
       </div>
+      // <div style={{maxWidth: '80%'}}>
+      //   <Video id='P5ktQh3sWZk' />
+      // </div>
     );
   }
 }
