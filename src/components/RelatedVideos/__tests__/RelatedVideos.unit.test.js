9875106ab56dc7fe59faf807422f928be8eaa29b
gionellef <gionellef@gmail.com>
2019-09-12 12:13:07 +0800

    Add tests for relatedvideos component

---
+++ src/components/RelatedVideos/__tests__/RelatedVideos.unit.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { RelatedVideos } from '../RelatedVideos';
+
+test('RelatedVideos', () => {
+  const wrapper = shallow (
+    <RelatedVideos />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
