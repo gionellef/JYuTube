1ba1034468aae9777bcd9c38e6ac5a73e00dda13
gionellef <gionellef@gmail.com>
2019-09-12 15:18:21 +0800

    Finish mockup of Comments component and its subcomponents

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -11,19 +11,19 @@ export function Rating(props) {
     const percent  = 100 * (props.likeCount / (props.likeCount + props.dislikeCount));
 
     progress = <Progress className='progress' percent={percent} size='tiny' />
-
-    return (
-      <div className='rating'>
-        <div className='thumbs-up'>
-          <Icon name='thumbs up outline' />
-          <span>{ props.likeCount }</span>
-        </div>
-        <div className='thumbs-down'>
-          <Icon name='thumbs down outline' />
-          <span>{ props.dislikeCount }</span>
-        </div>
-        { progress }
-      </div>
-    );
   }
+  
+  return (
+    <div className='rating'>
+      <div className='thumbs-up'>
+        <Icon name='thumbs up outline' />
+        <span>{ props.likeCount }</span>
+      </div>
+      <div className='thumbs-down'>
+        <Icon name='thumbs down outline' />
+        <span>{ props.dislikeCount }</span>
+      </div>
+      { progress }
+    </div>
+  );
 }
